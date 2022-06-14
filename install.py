072e5d7b24639fd6c3783816370525da580ba6ff
Techcable <Techcable@techcable.net>
2022-06-13 23:57:13 -0700

    Fixup install.py to properly support --target
    Didn't work before ^_^

--- install.py
+++ install.py
@@ -14,16 +14,22 @@ def main(flags):
     idx = 0
     target_path = "/opt/techcable"
     while idx < len(flags):
-        flag = flags[idx]
-        if flag == "--target":
+        flag_name = flags[idx]
+
+        if flag_name not in SUPPORTED_FLAGS:
+            print(f"Unknown flag: {flag_name}", file=sys.stderr)
+            sys.exit(1)
+        if flag_name == "--target":
             idx += 1
             try:
-                target_file = flag[idx]
+                target_path = flags[idx]
             except IndexError:
                 print("Must specify target", file=sys.stderr)
                 sys.exit(1)
+            else:
+                idx += 1
         else:
-            raise AssertionError(f"Unimplemetned flag: {flag!r}")
+            raise AssertionError
     assert binary.exists()
     print(f"Installing {binary} to {target_path}/bin/{binary.name}")
     bin_path = Path(target_path, "bin")
